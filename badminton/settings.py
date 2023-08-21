74ef2e8bcfb9c223003ac4d4a948bed6c5274ba3
shankar-0512 <shankar.narayananv7@gmail.com>
2023-08-21 14:30:45 +0100

    Update settings.py

--- badminton/settings.py
+++ badminton/settings.py
@@ -11,9 +11,15 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATIC_URL = '/static/'
 
 
 # Quick-start development settings - unsuitable for production
