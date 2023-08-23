6e34706aa6e45ef98e5edce5b42692f0237b3069
shankar-0512 <shankar.narayananv7@gmail.com>
2023-08-23 03:04:10 +0100

    Update routing.py

--- app/routing.py
+++ app/routing.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import consumers
 
 websocket_urlpatterns = [
-    path('wss/updates/', consumers.UpdateConsumer.as_asgi()),
+    path('ws/updates/', consumers.UpdateConsumer.as_asgi()),
 ]
