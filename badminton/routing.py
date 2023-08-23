98c190d9ae5aa193e6a94ad01a8d440792879721
shankar-0512 <shankar.narayananv7@gmail.com>
2023-08-23 03:59:57 +0100

    changes

--- badminton/routing.py
+++ badminton/routing.py
@@ -2,7 +2,6 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from django.urls import path
 from channels.auth import AuthMiddlewareStack
 from django.core.asgi import get_asgi_application
-
 import app.routing  # importing app-level routing
 
 application = ProtocolTypeRouter({
